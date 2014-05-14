eea968f64821952b0820ff515a315bb4b96ef8b5
Terence Lee <hone02@gmail.com>
2014-05-14 15:13:29 -0500

    add integration test

--- spec/certifi_spec.rb
+++ spec/certifi_spec.rb
@@ -6,6 +6,18 @@ describe Certifi do
     it "should return the path to the cacert.pem" do
       expect(File.exist?(Certifi.where)).to be_true
     end
+
+    it "should verify the cert" do
+      uri  = URI.parse('https://httpbin.org/')
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl     = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      http.ca_file     = Certifi.where
+      http.start do
+        http.request_get(uri.path) do |res|
+          expect(res.response).to be_kind_of(Net::HTTPOK)
+        end
+      end
     end
   end
 end
